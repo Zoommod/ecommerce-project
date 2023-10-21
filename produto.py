d9db6b6053beb0c6765fb5743ef77bd9b8ccd889
Zoommod <vitor.sacramento09@gmail.com>
2023-10-21 16:41:15 -0300

    ADD: primeiro commit

---
+++ produto.py
@@ -0,0 +1,15 @@
+class Produto:
+    def __init__(self, nome, codigo, descricao, valor):
+        self.nome = nome
+        self.codigo = codigo
+        self.descricao = descricao
+        self.valor = valor
+
+    def mostrar_Produtos(self):
+        print(f"Nome: {self.nome}")
+        print(f"Código: {self.codigo}")
+        print(f"Descrição: {self.descricao}")
+        print(f"Valor: {self.valor}")
+        print("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"*4)
+
+
