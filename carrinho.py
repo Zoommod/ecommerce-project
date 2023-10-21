23d6c31b0bf6a763ec2d1d6e7e4720a101b27bfd
Zoommod <vitor.sacramento09@gmail.com>
2023-10-21 17:59:40 -0300

    ADD: adicionando nova classe e dando função as opções 2, 3 e 4. Ainda não funcional

---
+++ carrinho.py
@@ -0,0 +1,16 @@
+class Carrinho:
+
+    def __init__(self):
+        self.lista_produtos = []
+        self.total = 0
+
+    def adicionar_Produto(self, produto):
+        self.lista_produtos.append(produto)
+        self.total += produto.valor
+        print(f"{produto.nome} adicionado ao carrinho.")
+    def mostrar_Carrinho(self):
+        print("CARRITO DE COMPRAS:")
+        for produto in self.lista_produtos:
+            print(f"Nome: {produto.nome}")
+            print(f"Valor: {produto.valor}")
+            print(f"Total: R${self.total:.2f}")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from produto import Produto
+from carrinho import Carrinho
 
 produto1 = Produto('Smartphone Galaxy S23 Ultra', 9540, 'O Samsung Galaxy S23 Ultra é um smartphone de ponta lançado pela Samsung, oferecendo uma variedade de recursos de alta qualidade. Ele apresenta um design elegante e uma tela AMOLED de alta resolução com taxa \nde atualização aprimorada para uma experiência visual excepcional.', 3149)
 produto2 = Produto('Monitor Samsung Odyssey Ark', 9541, 'O Monitor Samsung Odyssey Ark é uma tela de jogos de alto desempenho projetada especificamente para gamers. Ele oferece uma experiência de jogo imersiva com uma tela curva que envolve o campo de visão do\njogador, proporcionando uma sensação de maior profundidade e envolvimento', 3149)
@@ -6,6 +7,7 @@ produto3 = Produto('Placa de Vídeo RTX 3060 TI Ventus', 9542, 'A GeForce RTX 30
 produto4 = Produto('SSD 1 TB Kingston NV2, M.2 2280 PCIe', 9543, 'O NV2 PCIe 4.0 NVMe SSD da Kingston é uma solução substancial de armazenamento de última geração alimentada por um controlador Gen 4x4 NVMe. O NV2 oferece velocidades de leitura/gravação de até 3.500/2.100 MB/s\ncom menores requisitos de energia e menor aquecimento para ajudar a otimizar o desempenho do seu sistema e agregar valor sem sacrifícios.', 294)
 produto5 = Produto('Processador AMD Ryzen 7 5700X', 9544,'De maneira impressionante, os ganhos de desempenho da arquitetura “Zen 3” podem ser fornecidos sem aumento no consumo de energia ou TDP. A combinação de uma arquitetura de última geração com o processo de 7 nm\nlíder do setor dá ao AMD Ryzen série 5000 uma melhoria de + 24% de geração em eficiência energética e uma impressionante vantagem de 2,8X sobre as arquiteturas concorrentes.', 1248)
 
+carrinho = None
 while True:
     print("============================")
     print("[1] Visualizar Produtos")
@@ -15,11 +17,23 @@ while True:
     print("[5] Retirar Item")
     print("[6] Concluir Compra")
     print("============================")
-    opcao = input(str())
+    opcao = input(str("Escolha uma das opções: "))
     if opcao == '1':
         produto1.mostrar_Produtos()
         produto2.mostrar_Produtos()
         produto3.mostrar_Produtos()
         produto4.mostrar_Produtos()
         produto5.mostrar_Produtos()
-        break
+    elif opcao == '2':
+        if not carrinho:
+            carrinho = Carrinho()
+            print("O carrinho foi criado automaticamente!")
+        codigo_produto = str(input("Digite o código do produto a ser adicionado ao carrinho: "))
+        for produto in [produto1, produto2, produto3, produto4, produto5]:
+            if produto.codigo == codigo_produto:
+                carrinho.adicionar_Produto(produto)
+    elif opcao == '3':
+        carrinho = Carrinho()
+
+    elif opcao == '4':
+        carrinho.mostrar_Carrinho()
