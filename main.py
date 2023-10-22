b06e8a85275781cc8d1e042bd85a35f259abddcf
Zoommod <vitor.sacramento09@gmail.com>
2023-10-22 14:37:36 -0300

    ADD: opção de retirar item funcional e com tratamento de erros

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from produto import Produto, produto1, produto2, produto3, produto4, produto5
 from carrinho import Carrinho
 from cor import CorTexto
+
+
 lista_codigo_produtos = [produto1.get_codigo(), produto2.get_codigo(), produto3.get_codigo(), produto4.get_codigo(), produto5.get_codigo()]
 carrinho = None
 while True:
@@ -15,12 +17,14 @@ while True:
     opcao = input(str(f"{CorTexto.BRANCO}Escolha uma das opções: "))
     if opcao not in ['1', '2', '3', '4', '5', '6']:
         print(f"\n{CorTexto.VERMELHO}Opção digitada inválida. Tente novamente!")
+
     if opcao == '1':
         produto1.mostrar_produtos()
         produto2.mostrar_produtos()
         produto3.mostrar_produtos()
         produto4.mostrar_produtos()
         produto5.mostrar_produtos()
+
     elif opcao == '2':
         if not carrinho:
             carrinho = Carrinho()
@@ -31,14 +35,28 @@ while True:
         for produto in [produto1, produto2, produto3, produto4, produto5]:
             if produto.get_codigo() == codigo_produto:
                 carrinho.adicionar_produto(produto)
+
     elif opcao == '3':
         if not carrinho:
             carrinho = Carrinho()
             print("\nO carrinho foi criado com sucesso!")
         else:
             print(f"{CorTexto.VERMELHO}O carrinho já foi criado anteriormente!")
+
     elif opcao == '4':
         if not carrinho:
             print(f"{CorTexto.VERMELHO}Carrinho ainda não foi criado!")
         else:
             carrinho.mostrar_carrinho()
+
+    elif opcao == '5':
+        if not carrinho:
+            print(f"{CorTexto.VERMELHO}Carrinho ainda não foi criado!")
+        else:
+            codigo_produto = str(input("Digite o código do produto a ser removido do carrinho: "))
+            if codigo_produto not in carrinho.lista_codigos_carrinho:
+                print(
+                    f"\n{CorTexto.VERMELHO}O código digitado não é de um produto inserido no carrinho. Tente novamente!")
+            for produto in [produto1, produto2, produto3, produto4, produto5]:
+                if produto.get_codigo() == codigo_produto:
+                    carrinho.retirar_item(produto)
