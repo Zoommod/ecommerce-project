34041663fac9007927e2235595708b8b55764184
Zoommod <vitor.sacramento09@gmail.com>
2023-10-22 00:18:23 -0300

    FIX: problema ao adicionar produto ao carrinho solucionado

--- carrinho.py
+++ carrinho.py
@@ -12,5 +12,5 @@ class Carrinho:
         print("CARRITO DE COMPRAS:")
         for produto in self.lista_produtos:
             print(f"Nome: {produto.nome}")
-            print(f"Valor: {produto.valor}")
+            print(f"Valor: R${produto.valor}")
             print(f"Total: R${self.total:.2f}")
