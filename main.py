1238e925ed648e77dee61070c70e493fd7300b3f
Zoommod <vitor.sacramento09@gmail.com>
2023-10-27 08:36:23 -0300

    ADD: tratamento de erro relacionado a código inexistente adicionado no main.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from produto import Produto, produto1, produto2, produto3, produto4, produto5
 from carrinho import Carrinho
+from compra import Compra
 from cor import CorTexto
 
 
@@ -7,14 +8,14 @@ lista_codigo_produtos = [produto1.get_codigo(), produto2.get_codigo(), produto3.
 carrinho = None
 while True:
     print(f"{CorTexto.AZUL}============================")
-    print(f"{CorTexto.VERDE}[1] Visualizar Produtos")
-    print(f"{CorTexto.VERDE}[2] Adicionar Produto")
-    print(f"{CorTexto.VERDE}[3] Criar Carrinho")
-    print(f"{CorTexto.VERDE}[4] Visualizar Carrinho")
-    print(f"{CorTexto.VERDE}[5] Retirar Item")
-    print(f"{CorTexto.VERDE}[6] Concluir Compra")
+    print(f"{CorTexto.CIANO}[1] Visualizar Produtos")
+    print(f"{CorTexto.CIANO}[2] Adicionar Produto")
+    print(f"{CorTexto.CIANO}[3] Criar Carrinho")
+    print(f"{CorTexto.CIANO}[4] Visualizar Carrinho")
+    print(f"{CorTexto.CIANO}[5] Retirar Item")
+    print(f"{CorTexto.CIANO}[6] Concluir Compra")
     print(f"{CorTexto.AZUL}============================")
-    opcao = input(str(f"{CorTexto.BRANCO}Escolha uma das opções: "))
+    opcao = input(str(f"{CorTexto.CIANO}Escolha uma das opções: "))
     if opcao not in ['1', '2', '3', '4', '5', '6']:
         print(f"\n{CorTexto.VERMELHO}Opção digitada inválida. Tente novamente!")
 
@@ -28,8 +29,8 @@ while True:
     elif opcao == '2':
         if not carrinho:
             carrinho = Carrinho()
-            print("\nO carrinho foi criado automaticamente!")
-        codigo_produto = str(input("Digite o código do produto a ser adicionado ao carrinho: "))
+            print(f"\n{CorTexto.VERDE}O carrinho foi criado automaticamente!")
+        codigo_produto = str(input(f"{CorTexto.CIANO}Digite o código do produto a ser adicionado ao carrinho: "))
         if codigo_produto not in lista_codigo_produtos:
             print(f"\n{CorTexto.VERMELHO}O código digitado não consta em nosso banco de dados. Tente novamente!")
         for produto in [produto1, produto2, produto3, produto4, produto5]:
@@ -39,7 +40,7 @@ while True:
     elif opcao == '3':
         if not carrinho:
             carrinho = Carrinho()
-            print("\nO carrinho foi criado com sucesso!")
+            print(f"\n{CorTexto.VERDE}O carrinho foi criado com sucesso!")
         else:
             print(f"{CorTexto.VERMELHO}O carrinho já foi criado anteriormente!")
 
@@ -53,10 +54,14 @@ while True:
         if not carrinho:
             print(f"{CorTexto.VERMELHO}Carrinho ainda não foi criado!")
         else:
-            codigo_produto = str(input("Digite o código do produto a ser removido do carrinho: "))
-            if codigo_produto not in carrinho.lista_codigos_carrinho:
-                print(
-                    f"\n{CorTexto.VERMELHO}O código digitado não é de um produto inserido no carrinho. Tente novamente!")
-            for produto in [produto1, produto2, produto3, produto4, produto5]:
-                if produto.get_codigo() == codigo_produto:
-                    carrinho.retirar_item(produto)
+            codigo_produto = str(input(f"{CorTexto.CIANO}Digite o código do produto a ser removido do carrinho: "))
+            if codigo_produto not in lista_codigo_produtos:
+                print(f"\n{CorTexto.VERMELHO}O código digitado não é de um produto existente. Tente novamente!")
+            else:
+                for produto in [produto1, produto2, produto3, produto4, produto5]:
+                    if produto.get_codigo() == codigo_produto:
+                        carrinho.retirar_item(produto)
+
+    elif opcao == '6':
+        compra = Compra()
+        compra.cupom_fiscal(carrinho)
