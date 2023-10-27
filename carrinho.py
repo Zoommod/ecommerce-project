5399cf97d2d9888ab3f6eb49c7493d7e27c352e2
Zoommod <vitor.sacramento09@gmail.com>
2023-10-27 08:39:07 -0300

    ADD: agora o código que verifica se o id fornecido é o mesmo que foi adicionado ao carrinho foi migrado para o método

--- carrinho.py
+++ carrinho.py
@@ -1,5 +1,7 @@
-class Carrinho:
+from cor import CorTexto
 
+
+class Carrinho:
     def __init__(self):
         self.lista_produtos = []
         self.lista_codigos_carrinho = []
@@ -9,7 +11,7 @@ class Carrinho:
         self.lista_produtos.append(produto)
         self.lista_codigos_carrinho.append(produto.get_codigo())
         self.total += produto.get_valor()
-        print(f"\n'{produto.get_nome()}' adicionado ao carrinho.")
+        print(f"\n'{CorTexto.VERDE}{produto.get_nome()}' adicionado ao carrinho.")
 
     def mostrar_carrinho(self):
         print("CARRINHO DE COMPRAS:\n")
@@ -19,6 +21,14 @@ class Carrinho:
         print(f"Total: R${self.total:.2f}\n")
 
     def retirar_item(self, produto):
-        self.lista_codigos_carrinho.remove(produto.get_codigo())
-        self.lista_produtos.remove(produto)
-        self.total -= produto.get_valor()
+        if produto.get_codigo() in self.lista_codigos_carrinho:
+            self.lista_codigos_carrinho.remove(produto.get_codigo())
+            self.lista_produtos.remove(produto)
+            self.total -= produto.get_valor()
+            print(f"\n{CorTexto.VERDE}{produto.get_nome()} foi removido do carrinho.")
+        else:
+            print(f"\n{CorTexto.VERMELHO}O código digitado não é de um produto inserido no carrinho. Tente novamente!")
+
+
+
+
