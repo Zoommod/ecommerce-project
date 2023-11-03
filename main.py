d1b9b129a8a7b046227ee1e8cc60539795f04eed
Zoommod <vitor.sacramento09@gmail.com>
2023-11-03 19:57:25 -0300

    FIX: tratamento de erro adicinado ao escolher entre retirada e receber em casa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from produto import produto1, produto2, produto3, produto4, produto5
-from produto_terceiro import ProdutoTerceiro, produto6, produto7
+from produto_terceiro import produto6, produto7
 from compra_loja import CompraLoja
 from compra import Compra
 from cor import CorTexto
@@ -16,9 +16,10 @@ while True:
     print(f"{CorTexto.CIANO}[4] Visualizar Carrinho")
     print(f"{CorTexto.CIANO}[5] Retirar Item")
     print(f"{CorTexto.CIANO}[6] Concluir Compra")
+    print(f"{CorTexto.CIANO}[0] Sair do Programa")
     print(f"{CorTexto.AZUL}============================")
     opcao = input(str(f"{CorTexto.CIANO}Escolha uma das opções: "))
-    if opcao not in ['1', '2', '3', '4', '5', '6']:
+    if opcao not in ['0', '1', '2', '3', '4', '5', '6']:
         print(f"\n{CorTexto.VERMELHO}Opção digitada inválida. Tente novamente!")
 
     if opcao == '1':
@@ -37,7 +38,10 @@ while True:
 
         else:
             if not carrinho:
-                tipo_entrega = input(f"{CorTexto.CIANO}Digite '1' para entrega em casa ou '2' para retirada na loja: ")
+                tipo_entrega = None
+                while tipo_entrega not in ('1', '2'):
+                    tipo_entrega = input(f"{CorTexto.CIANO}Digite '1' para entrega em casa ou '2' para retirada na loja: ")
+                    print(f"{CorTexto.VERMELHO}Opção digitada inválida. Tente novamente!")
                 if tipo_entrega == '1':
                     carrinho = Compra()
                     print(f"\n{CorTexto.VERDE}O carrinho para entrega em casa foi criado com sucesso!")
@@ -91,3 +95,6 @@ while True:
         else:
             carrinho.concluir_compra()
             carrinho = None
+
+    elif opcao == '0':
+        exit()
